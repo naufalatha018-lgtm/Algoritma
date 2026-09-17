38a3a57e00bf46aa1817b372fcdea18b42ba2099
Naufal Athaillah <naufalatha018@gmail.com>
2026-09-17 11:27:22 +0700

    first commit

---
+++ Aman.java
@@ -0,0 +1,9 @@
+/**
+ *Program pertama
+ *Semua yang ada disini adalah komentar
+ */
+public class Aman {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
